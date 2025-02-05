29205ac578c508a344e5a32980f43a88bc017a02
Aggron282 <marcokhodr1@gmail.com>
2025-02-05 00:04:53 -0700

    S

--- util/sequelize_connection.js
+++ util/sequelize_connection.js
@@ -1,9 +1,10 @@
 var mysql2 = require("mysql2");
 var sequelize = require("sequelize");
+const isLocal = process.env.NODE_ENV !== 'production';
 
 var connection = new sequelize("colors","root","Linoone99!",{
   dialect:"mysql",
-  host:process.env.DB_HOST
+  host:isLocal ? process.env.LOCAL_DB_HOST : process.env.DB_HOST
 });
 
 module.exports = connection;
