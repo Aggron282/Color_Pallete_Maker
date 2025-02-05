e7c2613c3cc86d62ee10923e8ddef95618594a03
Aggron282 <marcokhodr1@gmail.com>
2025-02-05 00:14:50 -0700

    S

--- server.js
+++ server.js
@@ -24,14 +24,15 @@ const mysql = require('mysql2');
 const isLocal = process.env.NODE_ENV !== 'production';
 
 var options = {
-  config:{
-    host: isLocal ? process.env.LOCAL_DB_HOST: process.env.DB_HOST,
+  config: {
+    host: isLocal ? process.env.LOCAL_DB_HOST : process.env.DB_HOST,
     user: isLocal ? process.env.LOCAL_DB_USER : process.env.DB_USER,
     password: isLocal ? process.env.LOCAL_DB_PASSWORD : process.env.DB_PASSWORD,
     database: isLocal ? process.env.LOCAL_DB_NAME : process.env.DB_NAME,
-    port: isLocal ? process.env.DB_PORT : process.env.DB_PORT
+    port: isLocal ? process.env.LOCAL_DB_PORT : process.env.DB_PORT
   }
-}
+};
+
 
 
 // Configure session middleware
--- util/sequelize_connection.js
+++ util/sequelize_connection.js
@@ -2,9 +2,11 @@ var mysql2 = require("mysql2");
 var sequelize = require("sequelize");
 const isLocal = process.env.NODE_ENV !== 'production';
 
-var connection = new sequelize("colors","root","Linoone99!",{
-  dialect:"mysql",
-  host:isLocal ? process.env.LOCAL_DB_HOST : process.env.DB_HOST
+var connection = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: "mysql",
+  host: isLocal ? process.env.LOCAL_DB_HOST : process.env.DB_HOST,
+  port: isLocal ? process.env.LOCAL_DB_PORT : process.env.DB_PORT
 });
+
 
 module.exports = connection;
