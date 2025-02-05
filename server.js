4a9579c109bf7df31b2432a97c470f617195e9f4
Aggron282 <marcokhodr1@gmail.com>
2025-02-05 00:34:21 -0700

    S

--- server.js
+++ server.js
@@ -25,14 +25,13 @@ const isLocal = process.env.NODE_ENV !== 'production';
 
 var options = {
   config: {
-    host: isLocal ? process.env.LOCAL_DB_HOST : process.env.DB_HOST,
-    user: isLocal ? process.env.LOCAL_DB_USER : process.env.DB_USER,
-    password: isLocal ? process.env.LOCAL_DB_PASSWORD : process.env.DB_PASSWORD,
-    database: isLocal ? process.env.LOCAL_DB_NAME : process.env.DB_NAME,
-    port: isLocal ? process.env.LOCAL_DB_PORT : process.env.DB_PORT
+    host:  process.env.LOCAL_DB_HOST || process.env.DB_HOST,
+    user:  process.env.LOCAL_DB_USER || process.env.DB_USER,
+    password: process.env.LOCAL_DB_PASSWORD || process.env.DB_PASSWORD,
+    database: process.env.LOCAL_DB_NAME || process.env.DB_NAME,
+    port:  process.env.LOCAL_DB_PORT || process.env.DB_PORT
   }
 };
-
 
 
 // Configure session middleware
@@ -98,8 +97,11 @@ app.use(user_routes);
 app.use(routes);
 
 // Start the Sequelize connection and server
-sequelize_connection.sync().then(() => {
+// sequelize_connection.sync().then(() => {
+
   app.listen(port, () => {
+    console.log(options)
+
     console.log(`App is listening on port ${port}`);
   });
-});
+// });
--- util/sequelize_connection.js
+++ util/sequelize_connection.js
@@ -2,7 +2,7 @@ var mysql2 = require("mysql2");
 var sequelize = require("sequelize");
 const isLocal = process.env.NODE_ENV !== 'production';
 
-var connection = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+var connection = new sequelize(process.env.DB_NAME || "colors", process.env.DB_USER || "root", process.env.DB_PASSWORD || "Linoone99!", {
   dialect: "mysql",
   host: isLocal ? process.env.LOCAL_DB_HOST : process.env.DB_HOST,
   port: isLocal ? process.env.LOCAL_DB_PORT : process.env.DB_PORT
