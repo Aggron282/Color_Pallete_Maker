d5dbecb52c7cb31894fa36bbb27a70cc6fa7dea6
Aggron282 <marcokhodr1@gmail.com>
2025-02-05 00:39:57 -0700

    S

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ const mysql = require('mysql2');
 // Check if running locally or in a deployed environment
 const isLocal = process.env.NODE_ENV !== 'production';
 
-var options = {
+const options = {
   config: {
-    host:  process.env.LOCAL_DB_HOST || process.env.DB_HOST,
-    user:  process.env.LOCAL_DB_USER || process.env.DB_USER,
-    password: process.env.LOCAL_DB_PASSWORD || process.env.DB_PASSWORD,
-    database: process.env.LOCAL_DB_NAME || process.env.DB_NAME,
-    port:  process.env.LOCAL_DB_PORT || process.env.DB_PORT
+    host: isLocal ? process.env.LOCAL_DB_HOST : process.env.DB_HOST,
+    user: isLocal ? process.env.LOCAL_DB_USER : process.env.DB_USER,
+    password: isLocal ? process.env.LOCAL_DB_PASSWORD : process.env.DB_PASSWORD,
+    database: isLocal ? process.env.LOCAL_DB_NAME : process.env.DB_NAME,
+    port: isLocal ? process.env.LOCAL_DB_PORT : process.env.DB_PORT
   }
 };
 
@@ -97,11 +97,11 @@ app.use(user_routes);
 app.use(routes);
 
 // Start the Sequelize connection and server
-// sequelize_connection.sync().then(() => {
+sequelize_connection.sync().then(() => {
 
   app.listen(port, () => {
     console.log(options)
 
     console.log(`App is listening on port ${port}`);
   });
-// });
+});
--- util/sequelize_connection.js
+++ util/sequelize_connection.js
@@ -2,11 +2,16 @@ var mysql2 = require("mysql2");
 var sequelize = require("sequelize");
 const isLocal = process.env.NODE_ENV !== 'production';
 
-var connection = new sequelize(process.env.DB_NAME || "colors", process.env.DB_USER || "root", process.env.DB_PASSWORD || "Linoone99!", {
-  dialect: "mysql",
-  host: isLocal ? process.env.LOCAL_DB_HOST : process.env.DB_HOST,
-  port: isLocal ? process.env.LOCAL_DB_PORT : process.env.DB_PORT
-});
-
+const connection = new sequelize(
+  isLocal ? process.env.LOCAL_DB_NAME || "colors" : process.env.DB_NAME || "colors",
+  isLocal ? process.env.LOCAL_DB_USER || "root" : process.env.DB_USER || "root",
+  isLocal ? process.env.LOCAL_DB_PASSWORD || "Linoone99!" : process.env.DB_PASSWORD || "Linoone99!",
+  {
+    dialect: "mysql",
+    host: isLocal ? process.env.LOCAL_DB_HOST || "localhost" : process.env.DB_HOST,
+    port: isLocal ? process.env.LOCAL_DB_PORT || 3306 : process.env.DB_PORT || 3306,
+    logging: isLocal ? console.log : false, // Enables logging only in development
+  }
+);
 
 module.exports = connection;
