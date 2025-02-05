01e4a3d37a5b9ff894cda5ffb803498559347a90
Aggron282 <marcokhodr1@gmail.com>
2025-02-04 23:54:54 -0700

    S

--- util/sequelize_connection.js
+++ util/sequelize_connection.js
@@ -1,15 +1,6 @@
 var mysql2 = require("mysql2");
 var sequelize = require("sequelize");
 
-var options = {
-  config:{
-    host: isLocal ? process.env.DB_HOST: process.env.DB_HOST,
-    user: isLocal ? process.env.LOCAL_DB_USER : process.env.DB_USER,
-    password: isLocal ? process.env.LOCAL_DB_PASSWORD : process.env.DB_PASSWORD,
-    database: isLocal ? process.env.LOCAL_DB_NAME : process.env.DB_NAME,
-    port: isLocal ? process.env.DB_PORT : process.env.DB_PORT
-  }
-}
 var connection = new sequelize("colors","root","Linoone99!",{
   dialect:"mysql",
   host:process.env.DB_HOST
